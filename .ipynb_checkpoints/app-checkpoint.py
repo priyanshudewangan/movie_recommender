18ac774fba31cdc39fa0775c40812ad9d32374f3
Priyanshu Dewangan <chata@MacBook-Air.local>
2025-06-13 22:38:22 +0530

    Cleaned repo and added compressed file

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+def fetch_poster(movie_id):
+    response = requests.get(
+        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=582fc99f4c34db3acc0026bc82d09935&language=en-US'
+    )
+    data = response.json()
+    return "http://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distance)), reverse = True, key = lambda x: x[1])[1:6] 
+
+    recommended_movies = []
+    recommended_movies_poster = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
+
+
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    "Select a movie to get recommendations",
+    movies['title'].values
+)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+    
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
